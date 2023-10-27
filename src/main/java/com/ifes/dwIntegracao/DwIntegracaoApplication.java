7879cac7c46af3c838246b299fefd88afd2582ef
Rafael <rafaelmachadosouza3@gmail.com>
2023-10-27 20:47:55 -0300

    Implementado a classe TipoEpico.

--- src/main/java/com/ifes/dwIntegracao/DwIntegracaoApplication.java
+++ src/main/java/com/ifes/dwIntegracao/DwIntegracaoApplication.java
@@ -1,9 +1,14 @@
 package com.ifes.dwIntegracao;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@OpenAPIDefinition(info = @Info(title = "Trabalho de DW", version = "1.0", description = "Trabalho de desenvolvimento web."))
 public class DwIntegracaoApplication {
 
 	public static void main(String[] args) {
