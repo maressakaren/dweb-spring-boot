da3b771e55e6d350d2fe2dd5dd82242ab22bfd6b
maressakaren <khsmaressa@gmail.com>
2023-11-10 01:15:03 -0300

    correção epico

--- src/main/java/com/ifes/dwIntegracao/application/EpicoApplication.java
+++ src/main/java/com/ifes/dwIntegracao/application/EpicoApplication.java
@@ -2,12 +2,14 @@ package com.ifes.dwIntegracao.application;
 
 import com.ifes.dwIntegracao.dto.EpicoDTO;
 import com.ifes.dwIntegracao.exception.NotFoundException;
+import com.ifes.dwIntegracao.model.DependenciasTipoHU;
 import com.ifes.dwIntegracao.model.Epico;
 import com.ifes.dwIntegracao.repository.EpicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,7 +38,17 @@ public class EpicoApplication
             epico.setCategoria(categoriaApplication.retrieve(epicoDTO.getCategoria()));
             epico.setProjeto(projetoApplication.getById(epicoDTO.getProjeto_id()));
             epico.setTipoEpico(tipoEpicoApplication.retrieve(epicoDTO.getTipoEpico_id()));
-            epico.setDependencias(epicoDTO.getDependencias());
+            if (epicoDTO.getDependencias() != null) {
+                List<Epico> dependencias = new ArrayList<>();
+                for (Integer dependenciaId : epicoDTO.getDependencias()) {
+                    Epico dependencia = this.retrieve(dependenciaId);
+                    if (dependencia != null) {
+                        dependencias.add(dependencia);
+                    }
+                }
+                epico.setDependencias(dependencias);
+            }
+
             return repository.save(epico);
         } catch (NotFoundException e) {
             e.getMessage();
@@ -69,7 +81,17 @@ public class EpicoApplication
             epico.setTitulo(epicoDTO.getTitulo());
             epico.setDescricao(epicoDTO.getDescricao());
             epico.setRelevancia(epicoDTO.getRelevancia());
-            epico.setDependencias(epicoDTO.getDependencias());
+           // epico.setDependencias(epicoDTO.getDependencias());
+            if (epicoDTO.getDependencias() != null) {
+                List<Epico> dependencias = new ArrayList<>();
+                for (Integer dependenciaId : epicoDTO.getDependencias()) {
+                    Epico dependencia = this.retrieve(dependenciaId);
+                    if (dependencia != null) {
+                        dependencias.add(dependencia);
+                    }
+                }
+                epico.setDependencias(dependencias);
+            }
 
             return repository.save(epico);
         }
--- src/main/java/com/ifes/dwIntegracao/model/Relevancia.java
+++ src/main/java/com/ifes/dwIntegracao/model/Relevancia.java
@@ -5,18 +5,7 @@ import lombok.Getter;
 
 @Getter
 public enum Relevancia {
-    BAIXA("Baixa"),
-    MEDIA("Média"),
-    ALTA("Alta");
-
-    private final String descricao;
-
-    Relevancia(String descricao) {
-        this.descricao = descricao;
-    }
-
-    @Override
-    public String toString() {
-        return descricao;
-    }
+    BAIXA,
+    MEDIA,
+    ALTA;
 }
