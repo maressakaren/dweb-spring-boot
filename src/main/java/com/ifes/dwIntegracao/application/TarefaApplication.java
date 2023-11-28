4998156c4e29ef3031a656bd3c63b558df010991
Mateus <mateuslannes100@gmail.com>
2023-11-28 08:40:41 -0300

    adição do retrieve no metodo gera

--- src/main/java/com/ifes/dwIntegracao/application/TarefaApplication.java
+++ src/main/java/com/ifes/dwIntegracao/application/TarefaApplication.java
@@ -30,16 +30,16 @@ public class TarefaApplication {
         List<Tarefa> listaTarefa = new ArrayList<>();
         List<TipoTarefa> listaTTarefa = tipoTarefaApplication.getAll();
         try {
-            HistoriaUsuario historiaUser = huApplication.getById(id);
+            HistoriaUsuario historiaUser = huApplication.retrieve(id);// esta retrieve em vez de getById no TipoHistoriaUsuarioApplication
             if(listaTTarefa!= null){
                 for (TipoTarefa tipoTarefa : listaTTarefa) {
                     if(tipoTarefa.getTipoHistoriaUsuario() == historiaUser.getTipoHistoriaUsuario() ){ 
                         Tarefa tarefa = new Tarefa();                 
                         tarefa.setHistoriaUsuario(historiaUser); 
                         tarefa.setDescricao(historiaUser.getDescricao());
-                        String titulo = this.geraTitulo(historiaUser.getTitulo(), tipoTarefa.getDescricao()); 
-                        tarefa.setTitulo(titulo);
                         tarefa.setTipoTarefa(tipoTarefa);
+                        String titulo = this.geraTitulo(historiaUser.getTitulo(), tipoTarefa.getDescricao());
+                        tarefa.setTitulo(titulo);                        
                         listaTarefa.add(tarefa);
                     }
                 }
@@ -54,9 +54,9 @@ public class TarefaApplication {
     public String geraTitulo(String textoHistoriaUsuario, String tipoTarefa) {
 
         String[] palavras = textoHistoriaUsuario.split(" ");// Separa o texto da história de usuário em palavras
-        String[] palavrasTitulo = Arrays.copyOfRange(palavras, 3, palavras.length);// Remove as palavras indesejadas do início do título
-        palavrasTitulo[palavrasTitulo.length - 1] = tipoTarefa;// Adiciona o tipo de tarefa ao final do título
-        String titulo = String.join(" ", palavrasTitulo);// Junta as palavras do título em uma string
+        //String[] palavrasTitulo = Arrays.copyOfRange(palavras, 3, palavras.length);// Remove as palavras indesejadas do início do título
+        //palavrasTitulo[palavrasTitulo.length - 1] = tipoTarefa;// Adiciona o tipo de tarefa ao final do título
+        String titulo = String.join(" ", Arrays.copyOf(palavras, Math.min(4, palavras.length))) + " " + tipoTarefa + " " + palavras[palavras.length - 1];// Junta as palavras do título em uma string
     
         return titulo;
     }
@@ -93,7 +93,6 @@ public class TarefaApplication {
             e.getMessage();
         
         } 
-
     }
     public void delete(int id){
         repository.deleteById(id);
